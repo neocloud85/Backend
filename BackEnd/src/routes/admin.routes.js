2e767d5f4237a8047c1662b3c0c009bebc80d48b
neocloud85 <neocloud85@gmail.com>
2026-05-25 16:40:08 +0200

    Add files via upload

--- BackEnd/src/routes/admin.routes.js
+++ BackEnd/src/routes/admin.routes.js
@@ -8,7 +8,11 @@ import {
   updateUser,
   makeAdmin,
   getAllReviewsAdmin,
-  deleteReviewAdmin
+  deleteReviewAdmin,
+  getAllChatsAdmin,
+  deleteChatAdmin,
+  getChatMessagesAdmin,
+  deleteMessageAdmin
 } from '../controllers/admin.controller.js';
 
 const router = express.Router();
@@ -23,6 +27,15 @@ router.put('/panel/usuarios/:id', authMiddleware, adminMiddleware, updateUser);
 router.put('/panel/usuarios/:id/admin', authMiddleware, adminMiddleware, makeAdmin);
 router.get('/panel/resenas', getAllReviewsAdmin);
 router.delete('/panel/resenas/:id', deleteReviewAdmin);
+
+// CHATS (ADMIN)
+router.get('/panel/mensajes/chats', authMiddleware, adminMiddleware, getAllChatsAdmin);
+router.delete('/panel/mensajes/chats/:id', authMiddleware, adminMiddleware, deleteChatAdmin);
+
+// MENSAJES (ADMIN)
+router.get('/panel/mensajes/:chatId', authMiddleware, adminMiddleware, getChatMessagesAdmin);
+router.delete('/panel/mensajes/msg/:id', authMiddleware, adminMiddleware, deleteMessageAdmin);
+
 
 
 export const adminRoutes = router;
